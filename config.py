8d2036d10c2db88814e9974d58fae9416bbdcd18
billy0705 <billyg123@gmail.com>
2024-06-08 01:24:52 +0200

    added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Large Language Model name
+LLM_MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+# Embedding model name
+EMBEDDING_MODEL_NAME = "BAAI/bge-m3"
+
+# pdf folder path
+PDF_FOLDER_PATH = "./pdf_folder"
+
+# Vector database path
+DB_PATH = "./db"
+
+# Huggingface Token
+HF_TOKEN = None
